e81324bd15c896f747a4d537befe4200c7c4998f
Paul Suh <phs92@cornell.edu>
2022-06-04 16:24:31 -0700

    finish exercises 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...numbers) {
+  for (let i = 0; i < array.length; i ++) {
+    for (num of numbers){
+      if (array[i] === num){
+        array.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  console.log(array);
+  return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let sum = 0;
+  if (typeof num1 != "number" || typeof num2 != "number"
+  || num1 < 0 || num2 < 0){
+    return "ERROR";
+  }
 
+  let start;
+  let end;
+  if (num1 > num2) {
+    start = num2;
+    end = num1;
+  }
+  else{
+    start = num1;
+    end = num2;
+  }
+  for (let i = start; i <= end; i ++){
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
